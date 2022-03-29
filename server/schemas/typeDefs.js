a93dad7258f0c76894759d7adfa293bb7bd5092b
Zachary Clark <zacclark369@gmail.com>
2022-03-29 10:36:32 -0700

    added more to resolvers and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,8 +4,33 @@ const { gql} = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+}
+type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+}
+typer User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+}
 type Query {
-    helloWorld: String
+    users: [User]
+    user(usernam: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!) : Thought
 }
 `;
 
