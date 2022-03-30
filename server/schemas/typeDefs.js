475f472180cb47a714f054991d2e1039bf16e6ab
Zachary Clark <zacclark369@gmail.com>
2022-03-29 18:31:45 -0700

    completed mutations and auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,7 +18,7 @@ type Reaction {
     createdAt: String
     username: String
 }
-typer User {
+type User {
     _id: ID
     username: String
     email: String
@@ -27,10 +27,22 @@ typer User {
     friends: [User]
 }
 type Query {
+    me: User
     users: [User]
     user(usernam: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!) : Thought
+}
+type Mutation {
+    login(email: String!, password: String!) : Auth
+    addUser(username: String!, email: String!, password: String!) : Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+}
+type Auth {
+    token: ID!
+    user: User
 }
 `;
 
