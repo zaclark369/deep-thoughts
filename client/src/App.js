a35f27affb32ddf608869f7c039eb76c8019c3d0
Zachary Clark <zacclark369@gmail.com>
2022-03-31 14:55:22 -0700

    updated to be ready for implementing additions from users

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from "@apollo/client";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./pages/Login";
-import NoMatch from "./pages/NoMatch";
-import SingleThought from "./pages/SingleThought";
-import Profile from "./pages/Profile";
-import Signup from "./pages/Signup";
+} from '@apollo/client';
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import Home from "./pages/Home";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
@@ -29,22 +29,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        element as follows:
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/profile/:username?" component={Profile} />
-<Route exact path="/thought/:id" component={SingleThought} />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
 
-            <Route component={NoMatch} />
-          </Switch>
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,18 +2,16 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
-import FriendList from '../components/FriendList';
-
-
-const Profile = () => {
+const Profile = (props) => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParam }
+    variables: { username: userParam },
   });
 
   const user = data?.user || {};
@@ -31,18 +29,21 @@ const Profile = () => {
       </div>
 
       <div className="flex-row justify-space-between mb-3">
-  <div className="col-12 mb-3 col-lg-8">
-    <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
-  </div>
+        <div className="col-12 mb-3 col-lg-8">
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
+        </div>
 
-  <div className="col-12 col-lg-3 mb-3">
-    <FriendList
-      username={user.username}
-      friendCount={user.friendCount}
-      friends={user.friends}
-    />
-  </div>
-</div>
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
+        </div>
+      </div>
     </div>
   );
 };
